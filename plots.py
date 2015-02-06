d04e40b7caf0c31970572471cd706271858759a8
Henry Harrison <henry.schafer.harrison@gmail.com>
2015-02-05 22:29:59 -0500

    plotting stuff

--- plots.py
+++ plots.py
@@ -5,20 +5,37 @@ Usage:
 
 """
 import sys
+from operator import methodcaller
 
+from toolz import compose, curry
 import pandas as pd
 from experimentator import Experiment
 import matplotlib.pyplot as plt
 import seaborn as sns
 from docopt import docopt
 
-
-def transition_plot(df):
-    df['relative_height'] = df['height'] - df['lowest_height_not_afforded']
-    proportions = df.groupby()
+_phases = ('outbound', 'return')
 
 
-def load_data(filename, stack=True):
+def proportion(df, columns, phase='action'):
+    props = df.groupby(columns)[phase].apply(
+        compose(methodcaller('get', 'around', 0), methodcaller('value_counts', normalize=True))
+    )
+    props.name = 'p(around)'
+    return props
+
+
+def plot_proportions(df, by='relative_height'):
+    props = proportion(df, [by, 'width'])
+    with sns.color_palette(sns.cubehelix_palette()):
+        sns.FacetGrid(props.reset_index(), hue='width').map(plt.plot, by, 'p(around)', marker='o')
+
+    plt.legend(loc='lower right')
+    plt.xlabel('obstacle height {} lowest height not afforded'.format('-' if by == 'relative_height' else '/'))
+    plt.ylabel('P(around)')
+
+
+def load_data(filename, stack=True, collapse_unsuccesful=True):
     df = Experiment.load(filename).dataframe.dropna()
     for column, type_ in [
         ('height', 'int'),
@@ -28,10 +45,21 @@ def load_data(filename, stack=True):
     ]:
         df[column] = df[column].astype(type_)
 
-    if stack:
-        df = pd.concat((df, df), keys=['outbound', 'return'], names=['phase'])
-        df = df.reset_index().set_index(['participant', 'trial', 'phase']).sort()
+    df['relative_height'] = df['height'] - df['lowest_height_not_afforded']
+    df['scaled_height'] = df['height'] / df['lowest_height_not_afforded']
 
+    if collapse_unsuccesful:
+        for phase in _phases:
+            df.loc[df[phase] == 'unsuccessful', phase] = 'over'
+
+    if stack:
+        df = pd.concat((df, df), keys=_phases, names=['phase'])
+        df = df.reset_index().set_index(['participant', 'trial', 'phase']).sort()
+        df['action'] = [row[phase] for (p, t, phase), row in df.iterrows()]
+        for phase in _phases:
+            del df[phase]
+
+    return df
 
 def main():
     args = docopt(__doc__)
