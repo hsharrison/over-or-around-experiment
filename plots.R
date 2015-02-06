0252688eacc5f2659f04a53a6116bc028f7f26a1
Henry Harrison <henry.schafer.harrison@gmail.com>
2015-02-06 11:48:28 -0500

    improve plots.R

--- plots.R
+++ plots.R
@@ -9,22 +9,14 @@ ggplot(p,aes(x=rel_height, y=p_around, color=width, group=width)) +
   geom_point(size=3) +
   theme_pander() +
   scale_color_gradient(low='light green', high='black', name='width (ft.)') +
-  xlab('relative hieght (in. below max)') +
+  scale_x_continuous(breaks=c(-14, -10, -6, -2, 2)) +
+  xlab('obstacle height (in. relative to max)') +
   ylab('P(around)')
 ggsave('propotions.pdf')
 
 e <- na.omit(read.table('e1.csv', header=TRUE, sep=','))
-e <- reshape(e, direction='long',
-             varying=c('outbound', 'return'),
-             v.names='action',
-             timevar='phase',
-             times=c('outbound', 'return')
-)
-
-e[e$action == 'unsuccessful', 'action'] <- factor('over')
 e$action <- ordered(e$action, c('over', 'around'))
-e$rel_height <- e$height - e$lowest_height_not_afforded
-
+e$rel_height <- e$relative_height
 
 ggplot(e, aes(y=as.numeric(action)-1, x=rel_height, color=width, group=width)) +
   geom_point(position=position_jitter(h=0.07, w=1.15)) +
@@ -33,6 +25,6 @@ ggplot(e, aes(y=as.numeric(action)-1, x=rel_height, color=width, group=width)) +
   scale_y_continuous(breaks=c(0, 1), labels=c('over', 'around')) +
   scale_color_gradient(low='light green', high='black', name='width (ft.)') +
   theme_pander() +
-  xlab('relative hieght (in. below max)') +
+  xlab('obstacle height (in. relative to max)') +
   ylab('action')
 ggsave('every_trial.pdf')
