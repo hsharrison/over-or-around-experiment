56d8d968ec2ab47ebb6cc43d8a3ce1829e0ea302
Henry Harrison <henry.schafer.harrison@gmail.com>
2015-06-29 14:15:52 -0400

    simplify analysis

--- hysteresis.R
+++ hysteresis.R
@@ -7,21 +7,23 @@ library(extrafont)
 loadfonts()
 
 e <- read.table('e3.csv', header=TRUE, sep=',')
+e$h_max <- e$lowest_height_not_afforded
+e$lowest_height_not_afforded <- NULL
 e$action <- ordered(e$outbound, c('over', 'around'))
 e$order <- as.factor(e$sorted_order)
 e$height <- e$height + 1
-e$lowest_height_not_afforded <- e$lowest_height_not_afforded + 1
-e$relative_height <- e$height - e$lowest_height_not_afforded
-e$scaled_height <- e$height / e$lowest_height_not_afforded
+e$h_max <- e$h_max + 1
+e$relative_height <- e$height - e$h_max
+e$scaled_height <- e$height / e$h_max
 obstacle_distance <- 5
 e$tan_angle <- (e$width / 2) / obstacle_distance
 e$angle <- tan(e$tan_angle)
 # e <- e[e$participant != 2,]
 
-simple_glm <- function(d, predictor) {
-  glm(as.formula(paste('action', predictor, sep='~')), family=binomial(link='logit'), data=d)
+h_crit_glm <- function(d) {
+  glm(action ~ height, family=binomial(link='logit'), data = d)
 }
-p.glm <- function(mdl, p=.5) {
+p.glm <- function(mdl, p = .5) {
   a <- mdl %>% coef %>% extract(1)
   b <- mdl %>% coef %>% extract(2)
   result <- (log(p /(1-p)) - a) / b
@@ -32,37 +34,32 @@ sem <- function(x) {
   sd(x) / sqrt(length(x))
 }
 
-ddply.fun <- function(d, predictor, p=.5) {
+ddply.fun <- function(d, p = .5) {
   result <- d[1, apply(d, 2, . %>% unique %>% length %>% equals(1))]
-  new_col_name <- paste(predictor, p %>% as.character %>% strsplit('.', fixed=TRUE) %>% extract2(1) %>% extract(2), sep='.')
   
   if ('action' %in% names(result)) {
     result$switched <- FALSE
     
     if (result[1, 'action'] == 'over') {
       # Always went over. Set switching point to maximum height.
-      result[[new_col_name]] <- d[d$relative_height == 0, predictor]
+      result$h_crit <- result$h_max    
     } else {
       # Always went around. Set switching point to in-between the lowest height and what the next lowest would be if the sequence continued.
-      height <- min(d$height) - d$height %>% diff %>% mean %>% abs %>% divide_by(2)
-      if (predictor == 'scaled_height') {
-        result[[new_col_name]] <- height / result$lowest_height_not_afforded
-      } else if (predictor == 'relative_height') {
-        result[[new_col_name]] <- height - result$lowest_height_not_afforded
-      }
+      result$h_crit <- min(d$height) - d$height %>% diff %>% mean %>% abs %>% divide_by(2)
     }
     
   } else {
     result$switched <- TRUE
-    result[[new_col_name]] <- d %>% simple_glm(predictor) %>% p.glm(p=p)
+    result$h_crit <- d %>% h_crit_glm %>% p.glm
   }
   
   result$sorted_order <- NULL
   return(result)
 }
 
-rel_height.50 <- ddply(e, .(participant, block), . %>% ddply.fun('relative_height'))
-scaled_height.50 <- ddply(e, .(participant, block), . %>% ddply.fun('scaled_height'))
+data <- ddply(e, .(participant, block), ddply.fun)
+data$scaled_height.50 <- data$h_crit / data$h_max
+data$rel_height.50 <- data$h_crit - data$h_max
 
 fig.dims <- c(5, 5)
 font.family <- 'Ubuntu'
@@ -77,7 +74,7 @@ order_color <- scale_fill_few(palette='light')
 p_color <- scale_color_manual(values=c(few_pal('dark')(7), few_pal('medium')(5)), guide='none')
 width_offset <- 4
 
-ggplot(scaled_height.50, aes(x=width, y=scaled_height.5, fill=order, group=interaction(width, order))) +
+ggplot(data, aes(x=width, y=scaled_height.50, fill=order, group=interaction(width, order))) +
    stat_boxplot(position='dodge', width=8, size=1) +
    stat_summary(aes(group=interaction(width, participant), color=as.factor(participant),
                     x=width + width_offset/2 - width_offset*(order=='ascending')),
