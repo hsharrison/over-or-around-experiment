ff9784e71ecb99200dc6f33be9d81061b10761d7
Henry Harrison <henry.schafer.harrison@gmail.com>
2015-07-06 02:57:32 -0400

    more analysis

--- critical-heights-analysis.R
+++ critical-heights-analysis.R
@@ -4,9 +4,6 @@ library(ggplot2)
 library(extrafont)
 loadfonts()
 library(lme4)
-library(lmerTest)
-library(boot)
-library(tikzDevice)
 
 obstacle_distance <- 5
 
@@ -18,7 +15,8 @@ for (col in c('participant', 'phase', 'gender')) {
 e$tan_angle <- (e$width/2) / obstacle_distance
 e$angle <- atan(e$tan_angle)
 e$height <- e$height + 1
-e$lowest_height_not_afforded <- e$lowest_height_not_afforded + 1
+e$h_max <- e$lowest_height_not_afforded + 1
+e$lowest_height_not_afforded <- NULL
 
 n_return_actions <- with(subset(e, e$phase == 'return'), tapply(action, list(participant), . %>% unique %>% length))
 drop_return_participants <- names(n_return_actions[n_return_actions == 1])
@@ -27,8 +25,8 @@ e <- e[!(e$phase == 'return' & e$participant %in% drop_return_participants),]
 n_trials <- e$trial %>% unique %>% length
 e <- e[!with(e, participant == 15 & phase == 'outbound' & trial <= n_trials/2), ]
 
-simple_glm <- function(d, predictor) {
-  glm(as.formula(paste('action', predictor, sep='~')), family=binomial(link='logit'), data=d)
+h_crit_glm <- function(d) {
+  glm(action ~ height, family=binomial(link='logit'), data = d)
 }
 p.glm <- function(mdl, p=.5) {
   a <- mdl %>% coef %>% extract(1)
@@ -41,8 +39,34 @@ sem <- function(x) {
   sd(x) / sqrt(length(x))
 }
 
-rel_height.50 <- ddply(e, .(participant, width), . %>% simple_glm('relative_height') %>% p.glm)
-scaled_height.50 <- ddply(e, .(participant, width), . %>% simple_glm('scaled_height') %>% p.glm)
+ddply.fun <- function(d, p = .5) {
+  result <- d[1, apply(d, 2, . %>% unique %>% length %>% equals(1))]
+  
+  if ('action' %in% names(result)) {
+    result$switched <- FALSE
+    
+    if (result[1, 'action'] == 'over') {
+      # Always went over. Set switching point to maximum height.
+      result$h_crit <- result$h_max    
+    } else {
+      # Always went around. Set switching point to in-between the lowest height and what the next lowest would be if the sequence continued.
+      result$h_crit <- min(d$height) - d$height %>% diff %>% mean %>% abs %>% divide_by(2)
+    }
+    result$action <- NULL
+    
+  } else {
+    result$switched <- TRUE
+    result$h_crit <- d %>% h_crit_glm %>% p.glm
+  }
+  
+  result$sorted_order <- NULL
+  result$n <- nrow(d)
+  return(result)
+}
+
+data <- ddply(e, .(participant, width, phase), ddply.fun)
+data$scaled_height.50 <- data$h_crit / data$h_max
+data$rel_height.50 <- data$h_crit - data$h_max
 
 get_unique <- function(df, column) {
   df %>% extract(column) %>% unique %>% c %>% extract2(1)
@@ -68,7 +92,7 @@ no_x_grid <- theme(
   panel.grid.minor.x=element_blank()
 )
 
-ggplot(rel_height.50, aes(x=width, y=relative_height)) +
+ggplot(data, aes(x=width, y=rel_height.50)) +
   stat_summary(geom='bar', fun.y=mean, alpha=0.75) +
   stat_summary(geom='errorbar', width=0.5, size=1,
     fun.ymax=. %>% {mean(.) + sem(.)},
@@ -76,13 +100,8 @@ ggplot(rel_height.50, aes(x=width, y=relative_height)) +
   ) +
   width_x + crit_relative_height_y + plot_theme_bw + no_x_grid
 ggsave('critical_relative_heights.png', width=fig.dims[1], height=fig.dims[2])
-mer_rel_height <- lmer(relative_height ~ width + (1 + width | participant), rel_height.50)
-summary(mer_rel_height)
 
-scaled_height.50$tan_angle <- scaled_height.50$width / (10)
-scaled_height.50$angle <- atan(scaled_height.50$tan_angle) * 180/pi
-
-ggplot(scaled_height.50, aes(x=width, y=scaled_height)) +
+ggplot(data, aes(x=width, y=scaled_height.50)) +
   stat_summary(geom='bar', fun.y=mean, alpha=0.75) +
   stat_summary(geom='errorbar', width=0.5, size=1,
                fun.ymax=. %>% {mean(.) + sem(.)},
@@ -91,16 +110,103 @@ ggplot(scaled_height.50, aes(x=width, y=scaled_height)) +
   width_x + plot_theme_bw + no_x_grid + crit_scaled_height_y  + upright_ylabel +
   coord_cartesian(ylim=c(0.5, 1))
 ggsave('critical_scaled_heights.png', width=fig.dims[1], height=fig.dims[2])
-mer_scaled_height <- lmer(scaled_height ~ width + (1 + width | participant), scaled_height.50)
-summary(mer_scaled_height)
-without_width_raneff <- lmer(scaled_height ~ width + (1 | participant), scaled_height.50)
-anova(without_width_raneff, mer_scaled_height)
-summary(without_width_raneff)
 
-ci <- without_width_raneff %>%
-  bootMer(fixef, nsim=10000, type='parametric', .progress='txt') %>%
-  boot.ci(type='basic', index=2) %>%
-  extract2('basic') %>%
-  extract(4:5)
-print(c(ci[1], mean(ci), ci[2]))
-print(c(mean(ci), (ci[2] - ci[1]) / 2))
+cond_means <- lmer(h_crit ~ 1 + (1|participant), data, REML = FALSE)
+h_max <- lmer(h_crit ~ h_max + (1|participant), data, REML = FALSE)
+anova(cond_means, h_max, test = 'ChiSq')
+
+h_max_noint <- lmer(h_crit ~ -1 + h_max + (1|participant), data, REML = FALSE)
+anova(h_max_noint, h_max, test = 'ChiSq')
+
+means <- lmer(h_crit ~ -1 + (1|participant), data, REML = FALSE)
+anova(means, h_max_noint, test = 'ChiSq')
+
+age <- lmer(h_crit ~ -1 + age + (1|participant), data, REML = FALSE)
+anova(age, h_max_noint)
+
+knee_height <- lmer(h_crit ~ -1 + knee_height + (1|participant), data, REML = FALSE)
+anova(knee_height, h_max_noint)
+
+hip_height <- lmer(h_crit ~ -1 + hip_height + (1|participant), data, REML = FALSE)
+anova(knee_height, hip_height)
+
+both_heights <- lmer(h_crit ~ -1 + hip_height + knee_height + (1|participant), data, REML = FALSE)
+anova(hip_height, both_heights, test = 'ChiSq')
+
+hip_and_max <- lmer(h_crit ~ -1 + hip_height + h_max + (1|participant), data, REML = FALSE)
+anova(hip_height, hip_and_max, test= 'ChiSq')
+
+hip_and_int <- lmer(h_crit ~ 1 + hip_height + (1|participant), data, REML = FALSE)
+anova(hip_height, hip_and_int, test = 'ChiSq')
+
+hip_and_age <- lmer(h_crit ~ -1 + hip_height + age + (1|participant), data, REML = FALSE)
+anova(hip_height, hip_and_age, test = 'ChiSq')
+
+widths <- lmer(h_crit ~ -1 + hip_height + width + (1|participant), data, REML = FALSE)
+anova(hip_height, widths, test = 'ChiSq')
+
+angle <- lmer(h_crit ~ -1 + hip_height + angle + (1|participant), data, REML = FALSE)
+anova(widths, angle)
+
+tan_angle <- lmer(h_crit ~ -1 + hip_height + tan(angle) + (1|participant), data, REML = FALSE)
+anova(angle, tan_angle)
+
+angle_raneff <- lmer(h_crit ~ -1 + hip_height + angle + (1 + width|participant), data, REML = FALSE)
+anova(angle, angle_raneff, test = 'ChiSq')
+
+width_int <- lmer(h_crit ~ -1 + h_max*width + (1|participant), data, REML = FALSE)
+anova(widths, width_int, test = 'ChiSq')
+
+phase <- lmer(h_crit ~ -1 + hip_height + angle + phase + (1|participant), data, REML = FALSE)
+anova(angle, phase, test = 'ChiSq')
+
+gender <- lmer(h_crit ~ -1 + hip_height + angle + gender + (1|participant), data, REML = FALSE)
+anova(angle, gender, test = 'ChiSq')
+
+phase_raneff <- lmer(h_crit ~ -1 + hip_height + angle + (1 + phase|participant), data, REML = FALSE)
+anova(angle, phase_raneff, test = 'ChiSq')
+
+final <- phase_raneff
+summary(final)
+
+data$crit_hip_prop <- with(data, h_crit / hip_height)
+crit_hip_prop_y <- scale_y_continuous(name=expression(h[crit] / h[hip] ~~ plain('(in.)')))
+
+ggplot(data, aes(x=width, y=crit_hip_prop)) +
+  stat_summary(geom='bar', fun.y=mean, alpha=0.75) +
+  stat_summary(geom='errorbar', width=0.5, size=1,
+               fun.ymax=. %>% {mean(.) + sem(.)},
+               fun.ymin=. %>% {mean(.) - sem(.)}
+  ) +
+  width_x + crit_hip_prop_y + plot_theme_bw + no_x_grid +
+  coord_cartesian(ylim = c(0.35, 0.7))
+
+sub <- subset(data, data$width < 10)
+
+means <- lmer(h_crit ~ -1 + (1|participant), sub, REML = FALSE)
+int <- lmer(h_crit ~ 1 + (1|participant), sub, REML = FALSE)
+h_max <- lmer(h_crit ~ -1 + h_max + (1|participant), sub, REML = FALSE)
+knee <- lmer(h_crit ~ -1 + knee_height + (1|participant), sub, REML = FALSE)
+hip <- lmer(h_crit ~ -1 + hip_height + (1|participant), sub, REML = FALSE)
+age <- lmer(h_crit ~ -1 + age + (1|participant), sub, REML = FALSE)
+anova(means, int, h_max, knee, hip, age, test = 'ChiSq')
+best <- hip
+
+anova(best, update(best, . ~ . + 1), test = 'ChiSq')
+anova(best, update(best, . ~ . + knee_height), test = 'ChiSq')
+anova(best, update(best, . ~ . + age), test = 'ChiSq')
+anova(best, update(best, . ~ . + gender), test = 'ChiSq')
+anova(best, update(best, . ~ . + h_max), test = 'ChiSq')
+
+width <- update(best, . ~ . + width)
+angle <- update(best, . ~ . + angle)
+tan_angle <- update(best, . ~ . + tan_angle)
+anova(best, width, angle, tan_angle, test = 'ChiSq')
+best <- update(best, . ~ . + tan_angle)
+
+anova(best, update(best, . ~ . + (tan_angle|participant)), test = 'ChiSq')
+anova(best, update(best, . ~ . + tan_angle:hip_height), test = 'ChiSq')
+anova(best, update(best, . ~ . + phase), test = 'ChiSq')
+anova(best, update(best, . ~ . + (phase|participant)), test = 'ChiSq')
+
+summary(best)
